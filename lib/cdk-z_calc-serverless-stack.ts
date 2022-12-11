ad5138806daa624b4d5a053d7d8f3e6f94c5aa51
yethon <10037587+yethon@users.noreply.github.com>
2022-12-11 09:48:07 -0500

    CDK stack deployment function.

--- lib/cdk-z_calc-serverless-stack.ts
+++ lib/cdk-z_calc-serverless-stack.ts
@@ -1,16 +1,43 @@
 import * as cdk from 'aws-cdk-lib';
+import { CfnOutput } from 'aws-cdk-lib';
+import { Table } from 'aws-cdk-lib/aws-dynamodb';
+import { FunctionUrlAuthType, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dotenv from 'dotenv';
+import path = require('path');
+dotenv.config();
 
 export class CdkZCalcServerlessStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // DynamoDB table
+    // FYI - This table already exists, you'd do something else if you were creating it here.
+    const table = Table.fromTableArn(this, `${process.env.TABLE_NAME}`, `${process.env.TABLE_ARN}`);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkZCalcServerlessQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Lamda Function
+    const lFunc = new NodejsFunction(this, "ZCalculator", {
+      runtime: Runtime.NODEJS_18_X,
+      entry: path.join(__dirname, '../functions/zCalculator.ts'),
+      handler: "handler",
+      environment: {
+        TABLE_NAME: table.tableName
+      }
+    })
+
+    table.grantReadData(lFunc);
+
+    // API Gateway
+    const lUrl = lFunc.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ['*'],
+      }
+    });
+
+    new CfnOutput(this, 'lUrl', {
+      value: lUrl.url,
+    });
   }
 }
